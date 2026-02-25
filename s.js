83ec377ed58b9ddef33db4681eb965daab138925
Kiki Zhou <146445548+kiky97@users.noreply.github.com>
2026-02-25 16:16:21 +0100

    Add files via upload

---
+++ s.js
@@ -0,0 +1,49 @@
+const noteContainer = document.querySelector(".notes-contianer");
+const CreateBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+function showNotes(){
+    noteContainer.innerHTML=localStorage.getItem("notes");
+}
+
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes", noteContainer.innerHTML);
+
+}
+
+
+CreateBtn.addEventListener("click", ()=>{
+    let inputBox=document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute('contenteditable',"true");
+    img.src="images/delete.png";
+    noteContainer.appendChild(inputBox).appendChild(img);
+})
+
+noteContainer.addEventListener("click",function(e){
+    if(e.target.tagName=="IMG"){
+        e.target.parentElment.remove();
+    }
+    else if(e.target.tagName==="p"){
+        notes =document.querySelectorAll(".input-box");
+        notes.forEach(nt=>{
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+
+    }
+})
+
+
+document.addEventListener("keydown", event =>{
+    if(event.key ==="Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+}
+
+)
+
